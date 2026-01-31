851bcfdc2b5b961d1f7022c9521ab62ee86e3800
dogzone8 <dogzone00@gmail.com>
2026-01-30 21:18:27 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+/* Order List PWA Service Worker (V8 2026-01-30) */
+const CACHE_NAME = "orderlist-pwa-v9-2026-01-30";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-180.png",
+  "./icons/icon-167.png",
+  "./icons/icon-152.png",
+  "./icons/icon-120.png",
+  "./icons/icon-96.png",
+  "./icons/icon-72.png",
+  "./icons/icon-60.png",
+  "./icons/icon-48.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((k) => k.startsWith("orderlist-pwa-") && k !== CACHE_NAME)
+          .map((k) => caches.delete(k))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(req)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+          return resp;
+        })
+        .catch(() => cached);
+    })
+  );
+});
